186e8d44f20c7b8178d5fcefa16d6c4174b7c4f8
Nikhil Saraf <nsaraf@purestorage.com>
2020-11-15 04:41:18 -0800

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
 module.exports = {
 	future: {
-		removeDeprecatedGapUtilities: true,
 		purgeLayersByDefault: true,
 	},
-	purge: ["./lib/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+	purge: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {
-			colors: {
-				canvas: "#F6F9FA",
-				"canvas-grid": "#F6F9FA",
-			},
-		},
+		extend: {},
 	},
 	variants: {},
 	plugins: [],
