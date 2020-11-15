44c61b608884633fc3fcdd41d4803c1c3a034c46
Nikhil Saraf <nsaraf@purestorage.com>
2020-11-15 01:37:37 -0800

    fix some stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: ["./lib/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-};
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true,
+	},
+	purge: ["./lib/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				canvas: "#F6F9FA",
+				"canvas-grid": "#F6F9FA",
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+}
